36e8465a476c6b63833c599e893c82ebfe73cec2
Sascha Häberling <mail@sascha-haeberling.com>
2015-07-05 17:07:06 -0700

    Copyright change
    - Changes header to give copyright to all Winston authors.
    - These authors are specified in AUTHORS file.

--- winston-server/src/test/java/com/s13g/winston/tools/sauron/ImageRepositoryTest.java
+++ winston-server/src/test/java/com/s13g/winston/tools/sauron/ImageRepositoryTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2015 Sascha Haeberling
+ * Copyright 2015 The Winston Authors
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
